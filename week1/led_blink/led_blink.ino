5e6787f8faf670557de9cba930981046fcd383d0
RUBADHARSHINI S <rubadharshu123@gmail.com>
2026-07-02 17:30:25 +0530

    feat: add serial blink counter

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -7,8 +7,11 @@ Description : Blinks the built-in LED every 1 second.
 ----------------------------------------------------
 */
 
+int count = 0;
+
 void setup() {
   pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -17,4 +20,9 @@ void loop() {
 
   digitalWrite(13, LOW);
   delay(500);
+
+  count++;
+
+  Serial.print("Blink count: ");
+  Serial.println(count);
 }
