4ffe404b8c6895aa33063bec1bc6f510ab41c61e
RUBADHARSHINI S <rubadharshu123@gmail.com>
2026-07-03 22:47:58 +0530

    feat: add traffic light controller

---
+++ week2/traffic_light/traffic_light.ino
@@ -0,0 +1,64 @@
+/*
+  Traffic Light Controller with Pedestrian Button
+  Author: Your Name
+*/
+
+const int redLED = 2;
+const int yellowLED = 3;
+const int greenLED = 4;
+const int buttonPin = 7;
+
+void setup() {
+  pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  // Pedestrian button pressed
+  if (digitalRead(buttonPin) == LOW) {
+    digitalWrite(redLED, HIGH);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(greenLED, LOW);
+
+    Serial.print(millis());
+    Serial.println(" ms : Pedestrian Crossing - RED");
+
+    delay(8000);
+    return;
+  }
+
+  // RED
+  digitalWrite(redLED, HIGH);
+  digitalWrite(yellowLED, LOW);
+  digitalWrite(greenLED, LOW);
+
+  Serial.print(millis());
+  Serial.println(" ms : RED");
+
+  delay(5000);
+
+  // YELLOW
+  digitalWrite(redLED, LOW);
+  digitalWrite(yellowLED, HIGH);
+  digitalWrite(greenLED, LOW);
+
+  Serial.print(millis());
+  Serial.println(" ms : YELLOW");
+
+  delay(2000);
+
+  // GREEN
+  digitalWrite(redLED, LOW);
+  digitalWrite(yellowLED, LOW);
+  digitalWrite(greenLED, HIGH);
+
+  Serial.print(millis());
+  Serial.println(" ms : GREEN");
+
+  delay(4000);
+}
